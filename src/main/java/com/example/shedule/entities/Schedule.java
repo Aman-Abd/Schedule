54f9f89945af6c9e2d25970ce1321ae5ad5239b4
Aman-Abd <aman.abdyraxmanov@mail.ru>
2021-10-24 11:29:33 +0600

    Added Lombok & Swagger

--- src/main/java/com/example/shedule/entities/Schedule.java
+++ src/main/java/com/example/shedule/entities/Schedule.java
@@ -1,12 +1,16 @@
 package com.example.shedule.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
-import java.time.DayOfWeek;
 import java.time.LocalTime;
-import java.util.List;
 
 @Entity
 @Table(name="schedules")
+@Getter
+@Setter
+
 public class Schedule {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
@@ -23,46 +27,6 @@ public class Schedule {
         this.name = name;
         this.timeStart = timeStart;
         this.timeEnd = timeEnd;
-        this.dayOfWeek = dayOfWeek;
-    }
-
-    public int getId() {
-        return Id;
-    }
-
-    public void setId(int id) {
-        Id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalTime getTimeStart() {
-        return timeStart;
-    }
-
-    public void setTimeStart(LocalTime timeStart) {
-        this.timeStart = timeStart;
-    }
-
-    public LocalTime getTimeEnd() {
-        return timeEnd;
-    }
-
-    public void setTimeEnd(LocalTime timeEnd) {
-        this.timeEnd = timeEnd;
-    }
-
-    public String getDayOfWeek() {
-        return dayOfWeek;
-    }
-
-    public void setDayOfWeek(String dayOfWeek) {
         this.dayOfWeek = dayOfWeek;
     }
 }
