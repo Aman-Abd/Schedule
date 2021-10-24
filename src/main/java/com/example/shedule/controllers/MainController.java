17bc7703f7e508a4a820f5e2cff6cd46019dddd2
Aman-Abd <aman.abdyraxmanov@mail.ru>
2021-10-24 23:13:13 +0600

    Fix Swagger

--- src/main/java/com/example/shedule/controllers/MainController.java
+++ src/main/java/com/example/shedule/controllers/MainController.java
@@ -2,6 +2,8 @@ package com.example.shedule.controllers;
 
 import com.example.shedule.entities.Schedule;
 import com.example.shedule.repository.ScheduleRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,21 +12,25 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("schedule")
+@Api(value = "Schedule Controller class", description = "This class allows to interact with Schedule object")
 public class MainController {
     @Autowired
     private ScheduleRepository scheduleRepository;
 
     @GetMapping("{id}")
+    @ApiOperation(value = "Method to get schedule", response = List.class)
     public Optional<Schedule> getSchedule(@PathVariable int id){
         return scheduleRepository.findById(id);
     }
 
     @GetMapping
+    @ApiOperation(value = "Method to get schedules", response = List.class)
     public List<Schedule> getSchedules(){
         return scheduleRepository.findAll();
     }
 
     @PostMapping("")
+    @ApiOperation(value = "Method to add schedule", response = List.class)
     public void addSchedule(@RequestBody Schedule schedule){
         scheduleRepository.save(schedule);
     }
