82628564ce4d39bd85370bf1d0470bd693a11111
Aman-Abd <aman.abdyraxmanov@mail.ru>
2021-10-26 21:01:04 +0600

    added Hystrix

--- src/main/java/com/example/shedule/SheduleApplication.java
+++ src/main/java/com/example/shedule/SheduleApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableHystrix
 public class SheduleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/shedule/controllers/MainController.java
+++ src/main/java/com/example/shedule/controllers/MainController.java
@@ -2,6 +2,8 @@ package com.example.shedule.controllers;
 
 import com.example.shedule.entities.Schedule;
 import com.example.shedule.repository.ScheduleRepository;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,20 +21,52 @@ public class MainController {
 
     @GetMapping("{id}")
     @ApiOperation(value = "Method to get schedule", response = List.class)
+    @HystrixCommand(
+            fallbackMethod = "getScheduleFallBack",
+            threadPoolKey = "getSchedule",
+            threadPoolProperties = {
+                    @HystrixProperty(name="coreSize", value="100"),
+                    @HystrixProperty(name="maxQueueSize", value="50"),
+            })
     public Optional<Schedule> getSchedule(@PathVariable int id){
         return scheduleRepository.findById(id);
     }
 
+    public Optional<Schedule> getScheduleFallBack(@PathVariable int id){
+        return null;
+    }
+
     @GetMapping
     @ApiOperation(value = "Method to get schedules", response = List.class)
+    @HystrixCommand(
+            fallbackMethod = "getSchedulesFallBack",
+            threadPoolKey = "getSchedules",
+            threadPoolProperties = {
+                    @HystrixProperty(name="coreSize", value="100"),
+                    @HystrixProperty(name="maxQueueSize", value="50"),
+            })
     public List<Schedule> getSchedules(){
         return scheduleRepository.findAll();
     }
 
+    public List<Schedule> getSchedulesFallBack(){
+        return null;
+    }
+
     @PostMapping("")
     @ApiOperation(value = "Method to add schedule", response = List.class)
+    @HystrixCommand(
+            fallbackMethod = "addScheduleFallBack",
+            threadPoolKey = "addSchedule",
+            threadPoolProperties = {
+                    @HystrixProperty(name="coreSize", value="100"),
+                    @HystrixProperty(name="maxQueueSize", value="50"),
+            })
     public void addSchedule(@RequestBody Schedule schedule){
         scheduleRepository.save(schedule);
+    }
+
+    public void addScheduleFallBack(@RequestBody Schedule schedule){
     }
 
 }
